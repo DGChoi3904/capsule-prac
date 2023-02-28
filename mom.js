48ae365f53b3a638d70c8a011a7af24233c527f2
JangRuBin <wkdfnqls2465@gmail.com>
2023-02-28 13:08:06 +0900

    중간 저장

---
+++ mom.js
@@ -0,0 +1,25 @@
+const itsMe = {
+  name: "kongukjae",
+  age: 20,
+  home: "oracle",
+  hosil: "305ho",
+  sentence: "저는" + this.name + "입니다",
+  test: `저는 ${this.name}입니다`,
+
+  adult: function() {
+    if(this.age >= 20) {
+      console.log("나는 성인이야");
+    } else {
+      console.log("나는 아직 성인이 아니야");
+    }
+  },
+  nickName: function () {
+    this.name = "공미남"
+  }
+  
+};
+
+const korea = {
+  seoul : "capital",
+  
+}
